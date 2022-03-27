a3a29d8d530bb6d3f3daa3e707ddb16d36da1ec3
Kont Manstein <85468547+kontmanstein@users.noreply.github.com>
2022-03-27 08:00:10 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,23 @@
+const { AoiClient } require("aoi.js");
+
+const bot = new AoiClient({
+    token: process.env["token"],
+    intents: "all",
+    prefix: ["?","."]
+});
+
+bot.addEvent("onMessage");
+
+bot.commands.add("basicCommand", {
+    name: "ping",
+    code: `Pong! \`$pingms\``
+});
+
+bot.commands.load(`./komutlar/`);
+
+bot.status.add({
+    name: "Aoi.JS v6 Dev",
+    duration: 12000
+});
+
+bot.start();
